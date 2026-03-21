2fa0488983e6dba4c5731b4909d9348566ba2a3a
janier2000 <jomr10@hotmail.com>
2026-03-21 16:23:58 -0500

    1

---
+++ Orders/Orders.Backend/Controllers/CategoriesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Orders.Backend.UnitsOfWork.Interfaces;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : GenericController<Category>
+    {
+        public CategoriesController(IGenericUnitOfWork<Category> unitOfWork) : base(unitOfWork)
+        {
+            //_categoriesUnitOfWork = categoriesUnitOfWork;
+        }
+    }
+}
+
+//﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Orders.Backend.UnitsOfWork.Interfaces;
+//using Orders.Shared.DTOs;
+//using Orders.Shared.Entities;
+
+//namespace Orders.Backend.Controllers
+//{
+//    [ApiController]
+//    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+//    [Route("api/[controller]")]
+//    public class CategoriesController : GenericController<Category>
+//    {
+//        private readonly ICategoriesUnitOfWork _categoriesUnitOfWork;
+
+//        public CategoriesController(IGenericUnitOfWork<Category> unitOfWork, ICategoriesUnitOfWork categoriesUnitOfWork) : base(unitOfWork)
+//        {
+//            _categoriesUnitOfWork = categoriesUnitOfWork;
+//        }
+
+//        [AllowAnonymous]
+//        [HttpGet("combo")]
+//        public async Task<IActionResult> GetComboAsync()
+//        {
+//            return Ok(await _categoriesUnitOfWork.GetComboAsync());
+//        }
+
+//        [HttpGet]
+//        public override async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+//        {
+//            var response = await _categoriesUnitOfWork.GetAsync(pagination);
+//            if (response.WasSuccess)
+//            {
+//                return Ok(response.Result);
+//            }
+//            return BadRequest();
+//        }
+
+//        [HttpGet("totalPages")]
+//        public override async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+//        {
+//            var action = await _categoriesUnitOfWork.GetTotalPagesAsync(pagination);
+//            if (action.WasSuccess)
+//            {
+//                return Ok(action.Result);
+//            }
+//            return BadRequest();
+//        }
+//    }
+//}
