495b5a75084906056602117201e313ca60c4140f
janier2000 <jomr10@hotmail.com>
2026-03-21 16:39:45 -0500

    Repositories

---
+++ Orders/Orders.Backend/Repositories/Interface/IGenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Orders.Shared.Entities.Responses;
+
+namespace Orders.Backend.Repositories.Interface
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<ActionResponse<T>> GetAsync(int id);
+
+        Task<ActionResponse<IEnumerable<T>>> GetAsync();
+
+        //Task<ActionResponse<IEnumerable<T>>> GetAsync(PaginationDTO pagination);
+
+        //Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+
+        Task<ActionResponse<T>> AddAsync(T entity);
+
+        Task<ActionResponse<T>> DeleteAsync(int id);
+
+        Task<ActionResponse<T>> UpdateAsync(T entity);
+    }
+}
+
+
+//﻿using Orders.Shared.DTOs;
+//using Orders.Shared.Responses;
+
+//namespace Orders.Backend.Repositories.Interfaces
+//{
+//    public interface IGenericRepository<T> where T : class
+//    {
+//        Task<ActionResponse<T>> GetAsync(int id);
+
+//        Task<ActionResponse<IEnumerable<T>>> GetAsync();
+
+//        Task<ActionResponse<IEnumerable<T>>> GetAsync(PaginationDTO pagination);
+
+//        Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination);
+
+//        Task<ActionResponse<T>> AddAsync(T entity);
+
+//        Task<ActionResponse<T>> DeleteAsync(int id);
+
+//        Task<ActionResponse<T>> UpdateAsync(T entity);
+//    }
+//}
