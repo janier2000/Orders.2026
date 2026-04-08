d8f7e6949966c51f33f2615bf64d9aafc21631ea
janier2000 <jomr10@hotmail.com>
2026-04-07 19:01:03 -0500

    Tutorial 72 - Parte 52 - Almacenar foto en Azure

--- Orders/Orders.Backend/Controllers/AccountsController.cs
+++ Orders/Orders.Backend/Controllers/AccountsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Orders.Backend.Helpers;
 using Orders.Backend.UnitsOfWork.Interfaces;
 using Orders.Shared.DTOs;
 using Orders.Shared.Entities;
@@ -14,22 +15,20 @@ using System.Text;
 namespace Orders.Backend.Controllers
 {
     [ApiController]
-    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     public class AccountsController : ControllerBase
     {
         private readonly IUsersUnitOfWork _usersUnitOfWork;
         private readonly IConfiguration _configuration;
-        //private readonly IFileStorage _fileStorage;
+        private readonly IFileStorage _fileStorage;
         //private readonly IMailHelper _mailHelper;
         private readonly string _container;
 
-        public AccountsController(IUsersUnitOfWork usersUnitOfWork, IConfiguration configuration)//, IFileStorage fileStorage, IMailHelper mailHelper
+        public AccountsController(IUsersUnitOfWork usersUnitOfWork, IConfiguration configuration, IFileStorage fileStorage)
         {
             _usersUnitOfWork = usersUnitOfWork;
             _configuration = configuration;
-            //_fileStorage = fileStorage;
-            //_mailHelper = mailHelper;
+            _fileStorage = fileStorage;
             _container = "users";
         }
 
@@ -37,11 +36,11 @@ namespace Orders.Backend.Controllers
         public async Task<IActionResult> CreateUser([FromBody] UserDTO model)
         {
             User user = model;
-            //if (!string.IsNullOrEmpty(model.Photo))
-            //{
-            //    var photoUser = Convert.FromBase64String(model.Photo);
-            //    model.Photo = await _fileStorage.SaveFileAsync(photoUser, ".jpg", _container);
-            //}
+            if (!string.IsNullOrEmpty(model.Photo))
+            {
+                var photoUser = Convert.FromBase64String(model.Photo);
+                model.Photo = await _fileStorage.SaveFileAsync(photoUser, ".jpg", _container);
+            }
 
             var result = await _usersUnitOfWork.AddUserAsync(user, model.Password);
             if (result.Succeeded)
@@ -115,204 +114,6 @@ namespace Orders.Backend.Controllers
             };
         }
 
-
-        //[HttpGet("all")]
-        //public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
-        //{
-        //    var response = await _usersUnitOfWork.GetAsync(pagination);
-        //    if (response.WasSuccess)
-        //    {
-        //        return Ok(response.Result);
-        //    }
-        //    return BadRequest();
-        //}
-
-        //[HttpGet("totalPages")]
-        //public async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
-        //{
-        //    var action = await _usersUnitOfWork.GetTotalPagesAsync(pagination);
-        //    if (action.WasSuccess)
-        //    {
-        //        return Ok(action.Result);
-        //    }
-        //    return BadRequest();
-        //}
-        //[HttpPost("RecoverPassword")]
-        //public async Task<IActionResult> RecoverPasswordAsync([FromBody] EmailDTO model)
-        //{
-        //    var user = await _usersUnitOfWork.GetUserAsync(model.Email);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var myToken = await _usersUnitOfWork.GeneratePasswordResetTokenAsync(user);
-        //    var tokenLink = Url.Action("ResetPassword", "accounts", new
-        //    {
-        //        userid = user.Id,
-        //        token = myToken
-        //    }, HttpContext.Request.Scheme, _configuration["Url Frontend"]);
-
-        //    var response = _mailHelper.SendMail(user.FullName, user.Email!,
-        //        $"Orders - Recuperación de contraseña",
-        //        $"<h1>Orders - Recuperación de contraseña</h1>" +
-        //        $"<p>Para recuperar su contraseña, por favor hacer clic 'Recuperar Contraseña':</p>" +
-        //        $"<b><a href ={tokenLink}>Recuperar Contraseña</a></b>");
-
-        //    if (response.WasSuccess)
-        //    {
-        //        return NoContent();
-        //    }
-
-        //    return BadRequest(response.Message);
-        //}
-
-        //[HttpPost("ResetPassword")]
-        //public async Task<IActionResult> ResetPasswordAsync([FromBody] ResetPasswordDTO model)
-        //{
-        //    var user = await _usersUnitOfWork.GetUserAsync(model.Email);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var result = await _usersUnitOfWork.ResetPasswordAsync(user, model.Token, model.Password);
-        //    if (result.Succeeded)
-        //    {
-        //        return NoContent();
-        //    }
-
-        //    return BadRequest(result.Errors.FirstOrDefault()!.Description);
-        //}
-
-        //[HttpPost("ResedToken")]
-        //public async Task<IActionResult> ResedTokenAsync([FromBody] EmailDTO model)
-        //{
-        //    var user = await _usersUnitOfWork.GetUserAsync(model.Email);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var response = await SendConfirmationEmailAsync(user);
-        //    if (response.WasSuccess)
-        //    {
-        //        return NoContent();
-        //    }
-
-        //    return BadRequest(response.Message);
-        //}
-
-        //[HttpGet("ConfirmEmail")]
-        //public async Task<IActionResult> ConfirmEmailAsync(string userId, string token)
-        //{
-        //    token = token.Replace(" ", "+");
-        //    var user = await _usersUnitOfWork.GetUserAsync(new Guid(userId));
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var result = await _usersUnitOfWork.ConfirmEmailAsync(user, token);
-        //    if (!result.Succeeded)
-        //    {
-        //        return BadRequest(result.Errors.FirstOrDefault());
-        //    }
-
-        //    return NoContent();
-        //}
-
-        //[HttpPut]
-        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        //public async Task<IActionResult> PutAsync(User user)
-        //{
-        //    try
-        //    {
-        //        var currentUser = await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!);
-        //        if (currentUser == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        if (!string.IsNullOrEmpty(user.Photo))
-        //        {
-        //            var photoUser = Convert.FromBase64String(user.Photo);
-        //            user.Photo = await _fileStorage.SaveFileAsync(photoUser, ".jpg", _container);
-        //        }
-
-        //        currentUser.Document = user.Document;
-        //        currentUser.FirstName = user.FirstName;
-        //        currentUser.LastName = user.LastName;
-        //        currentUser.Address = user.Address;
-        //        currentUser.PhoneNumber = user.PhoneNumber;
-        //        currentUser.Photo = !string.IsNullOrEmpty(user.Photo) && user.Photo != currentUser.Photo ? user.Photo : currentUser.Photo;
-        //        currentUser.CityId = user.CityId;
-
-        //        var result = await _usersUnitOfWork.UpdateUserAsync(currentUser);
-        //        if (result.Succeeded)
-        //        {
-        //            return Ok(BuildToken(currentUser));
-        //        }
-
-        //        return BadRequest(result.Errors.FirstOrDefault());
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
-
-        //[HttpGet]
-        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        //public async Task<IActionResult> GetAsync()
-        //{
-        //    return Ok(await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!));
-        //}
-
-
-
-        //[HttpPost("changePassword")]
-        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        //public async Task<IActionResult> ChangePasswordAsync(ChangePasswordDTO model)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    var user = await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var result = await _usersUnitOfWork.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
-        //    if (!result.Succeeded)
-        //    {
-        //        return BadRequest(result.Errors.FirstOrDefault()!.Description);
-        //    }
-
-        //    return NoContent();
-        //}
-
-
-
-
-        //private async Task<ActionResponse<string>> SendConfirmationEmailAsync(User user)
-        //{
-        //    var myToken = await _usersUnitOfWork.GenerateEmailConfirmationTokenAsync(user);
-        //    var tokenLink = Url.Action("ConfirmEmail", "accounts", new
-        //    {
-        //        userid = user.Id,
-        //        token = myToken
-        //    }, HttpContext.Request.Scheme, _configuration["Url Frontend"]);
-
-        //    return _mailHelper.SendMail(user.FullName, user.Email!,
-        //        $"Orders - Confirmación de cuenta",
-        //        $"<h1>Orders - Confirmación de cuenta</h1>" +
-        //        $"<p>Para habilitar el usuario, por favor hacer clic 'Confirmar Email':</p>" +
-        //        $"<b><a href ={tokenLink}>Confirmar Email</a></b>");
-        //}
 
 
     }
