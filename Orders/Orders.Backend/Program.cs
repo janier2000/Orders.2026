00829100f3581261b71d001620e3f8a778c2638d
janier2000 <jomr10@hotmail.com>
2026-04-07 19:01:33 -0500

    Tutorial 72 - Parte 52 - Almacenar foto en Azure

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Orders.Backend.Data;
+using Orders.Backend.Helpers;
 using Orders.Backend.Repositories.Implementations;
 using Orders.Backend.Repositories.Interface;
 using Orders.Backend.Repositories.Interfaces;
@@ -69,7 +70,8 @@ builder
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
-builder.Services.AddTransient<SeedDb>(); 
+builder.Services.AddTransient<SeedDb>();
+builder.Services.AddScoped<IFileStorage, FileStorage>();
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
@@ -162,114 +164,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-//using System.Text;
-//using System.Text.Json.Serialization;
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.IdentityModel.Tokens;
-//using Microsoft.OpenApi.Models;
-//using Orders.Backend.Data;
-//using Orders.Backend.Helpers;
-//using Orders.Backend.Repositories.Implementations;
-//using Orders.Backend.Repositories.Interfaces;
-//using Orders.Backend.UnitsOfWork.Implementations;
-//using Orders.Backend.UnitsOfWork.Interfaces;
-//using Orders.Shared.Entities;
-//using Microsoft.Extensions.Azure;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-
-
-//builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
-//builder.Services.AddTransient<SeedDb>();
-//builder.Services.AddScoped<IFileStorage, FileStorage>();
-//builder.Services.AddScoped<IMailHelper, MailHelper>();
-//builder.Services.AddScoped<IOrdersHelper, OrdersHelper>();
-
-//builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-//builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
-
-//builder.Services.AddScoped<ICategoriesRepository, CategoriesRepository>();
-//builder.Services.AddScoped<ICitiesRepository, CitiesRepository>();
-//builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
-//builder.Services.AddScoped<IOrdersRepository, OrdersRepository>();
-//builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
-//builder.Services.AddScoped<IStatesRepository, StatesRepository>();
-//builder.Services.AddScoped<ITemporalOrdersRepository, TemporalOrdersRepository>();
-//builder.Services.AddScoped<IUsersRepository, UsersRepository>();
-
-//builder.Services.AddScoped<ICategoriesUnitOfWork, CategoriesUnitOfWork>();
-//builder.Services.AddScoped<ICitiesUnitOfWork, CitiesUnitOfWork>();
-//builder.Services.AddScoped<ICountriesUnitOfWork, CountriesUnitOfWork>();
-//builder.Services.AddScoped<IOrdersUnitOfWork, OrdersUnitOfWork>();
-//builder.Services.AddScoped<IProductsUnitOfWork, ProductsUnitOfWork>();
-//builder.Services.AddScoped<IStatesUnitOfWork, StatesUnitOfWork>();
-//builder.Services.AddScoped<ITemporalOrdersUnitOfWork, TemporalOrdersUnitOfWork>();
-//builder.Services.AddScoped<IUsersUnitOfWork, UsersUnitOfWork>();
-
-//builder.Services.AddIdentity<User, IdentityRole>(x =>
-//{
-//    x.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
-//    x.SignIn.RequireConfirmedEmail = true;
-//    x.User.RequireUniqueEmail = true;
-//    x.Password.RequireDigit = false;
-//    x.Password.RequiredUniqueChars = 0;
-//    x.Password.RequireLowercase = false;
-//    x.Password.RequireNonAlphanumeric = false;
-//    x.Password.RequireUppercase = false;
-//    x.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-//    x.Lockout.MaxFailedAccessAttempts = 3;
-//    x.Lockout.AllowedForNewUsers = true;
-//})
-//    .AddEntityFrameworkStores<DataContext>()
-//    .AddDefaultTokenProviders();
-
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//    .AddJwtBearer(x => x.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = false,
-//        ValidateAudience = false,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwtKey"]!)),
-//        ClockSkew = TimeSpan.Zero
-//    });
-//builder.Services.AddAzureClients(clientBuilder =>
-//{
-//    clientBuilder.AddBlobServiceClient(builder.Configuration["ConnectionStrings:AzureStorage:blob"]!, preferMsi: true);
-//    clientBuilder.AddQueueServiceClient(builder.Configuration["ConnectionStrings:AzureStorage:queue"]!, preferMsi: true);
-//});
-
-//var app = builder.Build();
-//SeedData(app);
-
-//void SeedData(WebApplication app)
-//{
-//    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
-
-//    using (var scope = scopedFactory!.CreateScope())
-//    {
-//        var service = scope.ServiceProvider.GetService<SeedDb>();
-//        service!.SeedAsync().Wait();
-//    }
-//}
-
-//app.UseCors(x => x
-//    .AllowAnyMethod()
-//    .AllowAnyHeader()
-//    .SetIsOriginAllowed(origin => true)
-//    .AllowCredentials());
-
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
-//app.MapControllers();
-//app.Run();
